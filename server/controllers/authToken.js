d9be00acf515d4177c3e77d19614b421ca1a576f
angshugit <am.mukherjee@gmail.com>
2018-09-25 13:27:34 -0600

    Get json web token from Authorization field of HTTP header as 'Bearer <token>'

--- server/controllers/authToken.js
+++ server/controllers/authToken.js
@@ -3,9 +3,23 @@ var config = require('../config'); // get our config file
 
 function authToken(req, res, next) {
     // check header/ url parameters or post parameters for token
-    var token = req.headers['x-access-token'];
-        if (!token) 
-            return res.status(403).send({ auth: false, message: 'No token provided.' });
+    var token = null;
+    if (req.headers.authorization &&
+        req.headers.authorization.split(' ')[0] === 'Bearer') {
+        // Authorization: Bearer g1jipjgi1ifjioj
+        // Handle token presented as a Bearer token in the Authorization header
+        console.log('Found Authorization Bearer token');
+        token = req.headers.authorization.split(' ')[1];
+    }
+
+    if (!token) {
+        token = req.headers['x-access-token'];
+    }
+
+    if (!token) {
+        return res.status(403).send({ auth: false, message: 'No token provided.' });
+    }
+
     // verifies secret and checks exp
     jwt.verify(token, config.salt, function(err, decoded) {      
         if (err) 
